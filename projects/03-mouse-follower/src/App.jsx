1687ecdc7daff45a810ebf23b4f9a226cdc38000
david-borja <davidborjamartinez@gmail.com>
2025-10-03 01:07:35 +0200

    feat(): project 03 and monorepo linter fixes

---
+++ projects/03-mouse-follower/src/App.jsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from 'react'
+
+function App () {
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+
+  useEffect(() => {
+    console.log('efecto')
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      setPosition({ x: clientX, y: clientY })
+    }
+
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    // cleanup:
+    // --> cuando el componente se desmonta
+    // --> cuando cambian las dependencias, antes de ejecutar el efecto de nuevo
+    return () => {
+      window.removeEventListener('pointermove', handleMove)
+    }
+
+    // truco para para debugguear suscripción a eventos: en la consola del navegador poner: getEventListeners(window) -> ahí podemos ver el número de veces que se ha suscrito a un evento. Si no se están limpiando, las suscripciones se acumulan. Debe haber solo una
+  }, [enabled])
+
+  return (
+    <main>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: '#09f',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -20,
+        top: -20,
+        width: 40,
+        height: 40,
+        transform: `translate(${position.x}px, ${position.y}px)`
+      }}
+      />
+      <button onClick={() => setEnabled(!enabled)}>{enabled ? 'Desactivar' : 'Activar'} seguir puntero</button>
+    </main>
+  )
+}
+
+export default App
