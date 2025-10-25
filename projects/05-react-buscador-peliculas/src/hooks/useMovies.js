8872dc854a10ed9b9495cb7003c7fa8748578504
david-borja <davidborjamartinez@gmail.com>
2025-10-25 12:27:22 +0200

    feat(): prevent duplicate movie searches

--- projects/05-react-buscador-peliculas/src/hooks/useMovies.js
+++ projects/05-react-buscador-peliculas/src/hooks/useMovies.js
@@ -1,15 +1,19 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import { searchMovies } from '../services/movies'
 
 export function useMovies ({ search }) {
   const [movies, setMovies] = useState([])
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState(null)
+  const previousSearch = useRef('')
 
   const getMovies = async () => {
+    if (search === previousSearch.current) return
+
     try {
       setLoading(true)
       setError(null)
+      previousSearch.current = search
       const searchedMovies = await searchMovies({ search })
       setMovies(searchedMovies)
     } catch (error) {
