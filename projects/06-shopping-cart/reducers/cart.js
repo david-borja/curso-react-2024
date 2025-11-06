98eb14bbbaf22b81a66476a682d39e2a98d0069c
david-borja <davidborjamartinez@gmail.com>
2025-11-06 18:11:50 +0100

    feat(): persist cart state in local storage and update on actions

--- projects/06-shopping-cart/reducers/cart.js
+++ projects/06-shopping-cart/reducers/cart.js
@@ -3,12 +3,17 @@
 // además, también nos permite testear la lógica de actualización del estado de forma aislada
 // Si hubiéramos dejado esta lógica en el contexto, tendríamos que montar todo el árbol de componentes con el provider para testearla
 
-export const cartInitialState = []
+const cartEmptyState = []
+export const cartInitialState = JSON.parse(window.localStorage.getItem('cart')) || cartEmptyState
 
 export const CART_ACTION_TYPES = {
   ADD_TO_CART: 'ADD_TO_CART',
   REMOVE_FROM_CART: 'REMOVE_FROM_CART',
   CLEAR_CART: 'CLEAR_CART'
+}
+
+export function updateLocalStorageCart (state) {
+  window.localStorage.setItem('cart', JSON.stringify(state))
 }
 
 const actions = {
@@ -20,19 +25,34 @@ const actions = {
     if (productInCartIndex >= 0) {
       const newCart = structuredClone(state)
       newCart[productInCartIndex].quantity += 1
+
+      // usando slice sería
+      // const newCart = [
+      //   ...state.slice(0, productInCartIndex),
+      //   {
+      //     ...state[productInCartIndex],
+      //     quantity: state[productInCartIndex].quantity + 1
+      //   },
+      //   ...state.slice(productInCartIndex + 1)
+      // ]
+      updateLocalStorageCart(newCart)
       return newCart
     }
 
     // producto no está en el carrito
     const newCart = [...state, { ...payload, quantity: 1 }]
+    updateLocalStorageCart(newCart)
     return newCart
   },
   [CART_ACTION_TYPES.REMOVE_FROM_CART]: (state, payload) => {
     const { id } = payload
-    return state.filter(item => item.id !== id)
+    const newCart = state.filter(item => item.id !== id)
+    updateLocalStorageCart(newCart)
+    return newCart
   },
   [CART_ACTION_TYPES.CLEAR_CART]: () => {
-    return cartInitialState
+    updateLocalStorageCart(cartEmptyState)
+    return cartEmptyState
   }
 }
 
