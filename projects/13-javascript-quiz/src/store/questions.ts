29e19909f19cd65c17ddc7cf8f64167f9bcbdef0
david-borja <davidborjamartinez@gmail.com>
2026-01-11 20:06:15 +0100

    feat(): integrate devtools middleware into questions store for enhanced debugging

--- projects/13-javascript-quiz/src/store/questions.ts
+++ projects/13-javascript-quiz/src/store/questions.ts
@@ -1,5 +1,5 @@
 import { create } from 'zustand'
-import { persist } from 'zustand/middleware'
+import { persist, devtools } from 'zustand/middleware'
 import { type Question } from '../../types'
 
 interface State {
@@ -25,51 +25,51 @@ interface State {
 // }
 
 export const useQuestionsStore = create<State>()(
-  // logger(
-  persist((set, get) => {
-    return {
-      questions: [],
-      currentQuestion: 0,
-      getQuestions: async (limit: number) => {
-        const res = await fetch('/data.json') // TO DO: esto lo extraeríamos a services
-        const json = await res.json()
-        const questions = json.sort(() => Math.random() - 0.5)
-        const limitedQuestions = limit ? questions.slice(0, limit) : questions
-        set({ questions: limitedQuestions })
-      },
-      selectAnswer: (questionId: number, answerIndex: number) => {
-        const { questions } = get()
-        const newQuestions = structuredClone(questions)
-        const questionIndex = newQuestions.findIndex(q => q.id === questionId)
-        const questionInfo = newQuestions[questionIndex]
-        const isCorrectUserAnswer = questionInfo.correctAnswer === answerIndex
-        newQuestions[questionIndex] = {
-          ...questionInfo,
-          isCorrectUserAnswer,
-          userSelectedAnswer: answerIndex
-        }
+  devtools(
+    persist((set, get) => {
+      return {
+        questions: [],
+        currentQuestion: 0,
+        getQuestions: async (limit: number) => {
+          const res = await fetch('/data.json') // TO DO: esto lo extraeríamos a services
+          const json = await res.json()
+          const questions = json.sort(() => Math.random() - 0.5)
+          const limitedQuestions = limit ? questions.slice(0, limit) : questions
+          set({ questions: limitedQuestions }, false, 'GET_QUESTIONS') // el segundo argumento es para indicar si se debe reemplazar el estado o mezclarlo, y el tercero es el nombre de la acción para devtools. Pero si no queremos las devtools, no haría falta ninguno de los dos params
+        },
+        selectAnswer: (questionId: number, answerIndex: number) => {
+          const { questions } = get()
+          const newQuestions = structuredClone(questions)
+          const questionIndex = newQuestions.findIndex(q => q.id === questionId)
+          const questionInfo = newQuestions[questionIndex]
+          const isCorrectUserAnswer = questionInfo.correctAnswer === answerIndex
+          newQuestions[questionIndex] = {
+            ...questionInfo,
+            isCorrectUserAnswer,
+            userSelectedAnswer: answerIndex
+          }
 
-        set({ questions: newQuestions })
-      },
-      goNextQuestion: () => {
-        const { currentQuestion, questions } = get()
-        const nextQuestion = currentQuestion + 1
-        if (nextQuestion < questions.length) {
-          set({ currentQuestion: nextQuestion })
-        }
-      },
-      goPrevQuestion: () => {
-        const { currentQuestion } = get()
-        const prevQuestion = currentQuestion - 1
-        if (prevQuestion >= 0) {
-          set({ currentQuestion: prevQuestion })
-        }
-      },
-      reset: () => set({ questions: [], currentQuestion: 0 }) 
-    }
-  },
-  { name: 'questions' }
-  ))
-//)
+          set({ questions: newQuestions })
+        },
+        goNextQuestion: () => {
+          const { currentQuestion, questions } = get()
+          const nextQuestion = currentQuestion + 1
+          if (nextQuestion < questions.length) {
+            set({ currentQuestion: nextQuestion })
+          }
+        },
+        goPrevQuestion: () => {
+          const { currentQuestion } = get()
+          const prevQuestion = currentQuestion - 1
+          if (prevQuestion >= 0) {
+            set({ currentQuestion: prevQuestion })
+          }
+        },
+        reset: () => set({ questions: [], currentQuestion: 0 }) 
+      }
+    },
+    { name: 'questions' }
+    ))
+)
 // por defecto lo guarda en el localStorage
 // pero se le puede indicar dónde guardarlo con: getStorage: () => sessionStorage, por ejemplo
