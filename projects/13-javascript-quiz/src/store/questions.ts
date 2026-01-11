bf9f51d5c1ac5641d603c6848edfeaace31ecd24
david-borja <davidborjamartinez@gmail.com>
2026-01-11 19:55:39 +0100

    feat(): commented logger as a middleware example

--- projects/13-javascript-quiz/src/store/questions.ts
+++ projects/13-javascript-quiz/src/store/questions.ts
@@ -12,49 +12,64 @@ interface State {
   reset: () => void;
 }
 
-export const useQuestionsStore = create<State>()(persist((set, get) => {
-  return {
-    questions: [],
-    currentQuestion: 0,
-    getQuestions: async (limit: number) => {
-      const res = await fetch('/data.json')
-      const json = await res.json()
-      const questions = json.sort(() => Math.random() - 0.5)
-      const limitedQuestions = limit ? questions.slice(0, limit) : questions
-      set({ questions: limitedQuestions })
-    },
-    selectAnswer: (questionId: number, answerIndex: number) => {
-      const { questions } = get()
-      const newQuestions = structuredClone(questions)
-      const questionIndex = newQuestions.findIndex(q => q.id === questionId)
-      const questionInfo = newQuestions[questionIndex]
-      const isCorrectUserAnswer = questionInfo.correctAnswer === answerIndex
-      newQuestions[questionIndex] = {
-        ...questionInfo,
-        isCorrectUserAnswer,
-        userSelectedAnswer: answerIndex
-      }
+// const logger = (config) => (set, get, api) => {
+//   return config(
+//     // este es el set modificado
+//     (...args) => {
+//       console.log('  applying', args)
+//       set(...args)
+//       console.log('  new state', get())
+//     },
+//     get,
+//     api)
+// }
 
-      set({ questions: newQuestions })
-    },
-    goNextQuestion: () => {
-      const { currentQuestion, questions } = get()
-      const nextQuestion = currentQuestion + 1
-      if (nextQuestion < questions.length) {
-        set({ currentQuestion: nextQuestion })
-      }
-    },
-    goPrevQuestion: () => {
-      const { currentQuestion } = get()
-      const prevQuestion = currentQuestion - 1
-      if (prevQuestion >= 0) {
-        set({ currentQuestion: prevQuestion })
-      }
-    },
-    reset: () => set({ questions: [], currentQuestion: 0 }) 
-  }
-},
-{ name: 'questions' }
-))
+export const useQuestionsStore = create<State>()(
+  // logger(
+  persist((set, get) => {
+    return {
+      questions: [],
+      currentQuestion: 0,
+      getQuestions: async (limit: number) => {
+        const res = await fetch('/data.json') // TO DO: esto lo extraeríamos a services
+        const json = await res.json()
+        const questions = json.sort(() => Math.random() - 0.5)
+        const limitedQuestions = limit ? questions.slice(0, limit) : questions
+        set({ questions: limitedQuestions })
+      },
+      selectAnswer: (questionId: number, answerIndex: number) => {
+        const { questions } = get()
+        const newQuestions = structuredClone(questions)
+        const questionIndex = newQuestions.findIndex(q => q.id === questionId)
+        const questionInfo = newQuestions[questionIndex]
+        const isCorrectUserAnswer = questionInfo.correctAnswer === answerIndex
+        newQuestions[questionIndex] = {
+          ...questionInfo,
+          isCorrectUserAnswer,
+          userSelectedAnswer: answerIndex
+        }
+
+        set({ questions: newQuestions })
+      },
+      goNextQuestion: () => {
+        const { currentQuestion, questions } = get()
+        const nextQuestion = currentQuestion + 1
+        if (nextQuestion < questions.length) {
+          set({ currentQuestion: nextQuestion })
+        }
+      },
+      goPrevQuestion: () => {
+        const { currentQuestion } = get()
+        const prevQuestion = currentQuestion - 1
+        if (prevQuestion >= 0) {
+          set({ currentQuestion: prevQuestion })
+        }
+      },
+      reset: () => set({ questions: [], currentQuestion: 0 }) 
+    }
+  },
+  { name: 'questions' }
+  ))
+//)
 // por defecto lo guarda en el localStorage
 // pero se le puede indicar dónde guardarlo con: getStorage: () => sessionStorage, por ejemplo
